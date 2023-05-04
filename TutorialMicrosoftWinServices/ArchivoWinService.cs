a48f1907bb06998f8661e863daeb8591c7e6847f
Christopher Morales <cmorales@sistemas-iq.com>
2023-05-04 09:42:13 -0600

    Sondeo y Registro de estados
    Sondeo del sistema cada 60s, y registro del estado del sistema

--- TutorialMicrosoftWinServices/ArchivoWinService.cs
+++ TutorialMicrosoftWinServices/ArchivoWinService.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers; // Para sondeo sencillo del sistema
 
 namespace TutorialMicrosoftWinServices
 {
@@ -24,14 +26,49 @@ namespace TutorialMicrosoftWinServices
             }
             RegistroEventos1.Source = "MiFuente";
             RegistroEventos1.Log = "MiNuevoRegsitro";
+
+        }
+        // Identificador de eventos
+        private int ID_evento = 1;
+        // Método OnTimer: Actividades que se ejecutan al sondear con TempSondeo (cada 60s)
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            // Aquí se insertan las actividades de monitoreo
+            RegistroEventos1.WriteEntry("Monitoreando el sistema: ", EventLogEntryType.Information, ID_evento++);
         }
 
         protected override void OnStart(string[] args)
         {
+            // Registro del estado OnStart
+            RegistroEventos1.WriteEntry("Estado: OnStart.");
+
+            // Sondeo del sistema
+            // Temporizador
+            Timer TempSondeo = new Timer();
+            TempSondeo.Interval = 60000; //60 segundos
+            TempSondeo.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            TempSondeo.Start();
         }
 
         protected override void OnStop()
         {
+            // Registro del estado OnStop
+            RegistroEventos1.WriteEntry("Estado: OnStop.");
+        }
+        protected override void OnContinue()
+        {
+            // Registro del estado OnContinue
+            RegistroEventos1.WriteEntry("Estado: OnContinue.");
+        }
+        protected override void OnPause()
+        {
+            // Registro del estado OnPause
+            RegistroEventos1.WriteEntry("Estado: OnPause.");
+        }
+        protected override void OnShutdown()
+        {
+            // Registro del estado OnShutdown
+            RegistroEventos1.WriteEntry("Estado: OnShutdown.");
         }
     }
 }
