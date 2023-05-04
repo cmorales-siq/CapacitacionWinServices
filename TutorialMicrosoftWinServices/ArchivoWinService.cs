dd2c52c7a72b164ce39ac9bfd74befc7140eb610
Christopher Morales <cmorales@sistemas-iq.com>
2023-05-04 10:06:43 -0600

    Estados Pendientes del Servicio
    Para los estados OnStart y OnStop

--- TutorialMicrosoftWinServices/ArchivoWinService.cs
+++ TutorialMicrosoftWinServices/ArchivoWinService.cs
@@ -9,6 +9,30 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers; // Para sondeo sencillo del sistema
+using System.Runtime.InteropServices; // Para estados pendientes
+
+public enum ServiceState // Valores del Estado del Servicio
+{
+    SERVICE_STOPPED = 0x00000001,
+    SERVICE_START_PENDING = 0x00000002,
+    SERVICE_STOP_PENDING = 0x00000003,
+    SERVICE_RUNNING = 0x00000004,
+    SERVICE_CONTINUE_PENDING = 0x00000005,
+    SERVICE_PAUSE_PENDING = 0x00000006,
+    SERVICE_PAUSED = 0x00000007,
+}
+[StructLayout(LayoutKind.Sequential)]
+public struct ServiceStatus // Estructura utilizada en una llamada de invocación de plataforma
+{
+    public int dwServiceType;
+    public ServiceState dwCurrentState;
+    public int dwControlsAccepted;
+    public int dwWin32ExitCode;
+    public int dwServiceSpecificExitCode;
+    public int dwCheckPoint; // Utilizado por Administrador de servicios
+    public int dwWaitHint;   // Utilizado por Administrador de servicios
+};
+
 
 namespace TutorialMicrosoftWinServices
 {
@@ -26,7 +50,6 @@ namespace TutorialMicrosoftWinServices
             }
             RegistroEventos1.Source = "MiFuente";
             RegistroEventos1.Log = "MiNuevoRegsitro";
-
         }
         // Identificador de eventos
         private int ID_evento = 1;
@@ -37,8 +60,18 @@ namespace TutorialMicrosoftWinServices
             RegistroEventos1.WriteEntry("Monitoreando el sistema: ", EventLogEntryType.Information, ID_evento++);
         }
 
+        // Función SetServiceStatus para invocación de plataforma
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(System.IntPtr handle, ref ServiceStatus serviceStatus);
+
         protected override void OnStart(string[] args)
         {
+            // Actualizar estado del estado a "pendiente corriendo"
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
             // Registro del estado OnStart
             RegistroEventos1.WriteEntry("Estado: OnStart.");
 
@@ -48,12 +81,26 @@ namespace TutorialMicrosoftWinServices
             TempSondeo.Interval = 60000; //60 segundos
             TempSondeo.Elapsed += new ElapsedEventHandler(this.OnTimer);
             TempSondeo.Start();
+
+            // Actualizar estado del servicio a "corriendo"
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
         }
 
         protected override void OnStop()
         {
+            // Actualizar estado del estado a "pendiente parado"
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOP_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
             // Registro del estado OnStop
             RegistroEventos1.WriteEntry("Estado: OnStop.");
+
+            // Actualizar estado del servicio a "parado"
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOPPED;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
         }
         protected override void OnContinue()
         {
